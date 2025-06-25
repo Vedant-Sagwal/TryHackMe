2827eecb7531e82864787ca639f3a2e0f4191e26
vedantsagwal <vedantsagwaldebain@vedantsagwal.vedantsagwak>
2025-06-25 13:42:59 +0530

    shhhh

---
+++ VsPortScanner/scanner.py
@@ -0,0 +1,20 @@
+import socket
+
+def scan(ports, target) :
+    print(f"Scanning ports of {target}")
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        try:
+            result = sock.connect_ex((target, port))
+            if result == 0:
+                print(f"(+) port {port} is open")
+        except (ex):
+            print(f"(-) error in scanning {port} port")
+        finally:
+            sock.close()
+
+if __name__ == "__main__":
+    target = input("Enter ip or target name : ")
+    ports = range(1, 5000)
+    scan(ports, target);
